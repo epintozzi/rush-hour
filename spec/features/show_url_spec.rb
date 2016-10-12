fcd43022f87e3503a0389566638e798810010e7e
Alisher Sadikov <alishersadikov@gmailcom>
2016-10-12 13:31:15 -0600

    show_url tests added

--- spec/features/show_url_spec.rb
+++ spec/features/show_url_spec.rb
@@ -18,4 +18,25 @@ RSpec.describe "When a valid user views URL data" do
       expect(page).to have_content("URL: about")
     end
   end
+
+  it "Shows url stats" do
+    new_client
+    new_payload
+    second_payload
+    visit("/sources/google/urls/about")
+    within("#url_stats") do
+      expect(page).to have_content("Max Response Time: 90")
+      expect(page).to have_content("Min Response Time: 32")
+      expect(page).to have_content("Longest To Shortest Response Time: [90, 32]")
+      expect(page).to have_content("Average Response Time: 61")
+      expect(page).to have_content('List of HTTP Verbs: ')
+      expect(page).to have_content('GET')
+      expect(page).to have_content('Three Most Popular Referrers: ')
+      expect(page).to have_content("http://apple.com")
+      expect(page).to have_content("http://apple.com/research")
+      expect(page).to have_content('Three Most Popular User Agents: ')
+      expect(page).to have_content("Chrome")
+      expect(page).to have_content("Macintosh")
+    end
+  end
 end
